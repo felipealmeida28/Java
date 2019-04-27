6bf86074a297f5ffba2d5bda7a81de2d1078e154
Felipe Almeida <felipealmeida281997@hotmail.com>
2019-04-27 01:13:00 -0300

    Limpeza da classe Principal

--- ContaCorrente/src/TestaConta.java
+++ ContaCorrente/src/TestaConta.java
@@ -2,18 +2,14 @@ public class TestaConta{
 
 
     public static void main(String[]args){
-        Conta c1 = new Conta("Felipe Almeida", "9112777", 128881,"28/10/2000");
-        Conta c2 = new Conta("Felipe Almeida", "9112777", 128881,"28/10/2000");
-        System.out.println("Salario Antigo: " + c1.getSaldo());
-        c1.setSaldo(9000);
-        c1.Deposita(c1,101);
-        c1.Transfere(c2, 9100);
 
-        System.out.println(c1.getSaldo());
+        
 
-        c1.Saca(2);
 
-        System.out.println(c1.getSaldo());
+
+
+
+
 
 
     }
